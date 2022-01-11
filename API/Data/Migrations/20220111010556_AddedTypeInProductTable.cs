c6942f812782e7ee7a5b98c719d8962e6a2482c4
trktuhin <robin35-908@diu.edu.bd>
2022-01-11 08:25:35 +0600

    End of section 2

---
+++ API/Data/Migrations/20220111010556_AddedTypeInProductTable.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class AddedTypeInProductTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "Products",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Products");
+        }
+    }
+}
